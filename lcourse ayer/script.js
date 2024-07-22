66b66f0f8eed9229b374db8d3b458cefd193eab7
Dawit <116512754+Dawa-22@users.noreply.github.com>
2024-07-22 23:05:58 +0300

    Add files via upload

---
+++ lcourse ayer/script.js
@@ -0,0 +1,107 @@
+function myFun1() {
+  var custDiv = document.getElementById("vid-wrap1");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
+
+function myFunn1() {
+  var custDiv = document.getElementById("not-wrap1");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
+
+function myFunny1() {
+  var custDiv = document.getElementById("eg-wrap1");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
+
+function myFun2() {
+  var custDiv = document.getElementById("vid-wrap2");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
+
+function myFunn2() {
+  var custDiv = document.getElementById("not-wrap2");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
+
+function myFunny2() {
+  var custDiv = document.getElementById("eg-wrap2");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
+
+function myFun3() {
+  var custDiv = document.getElementById("vid-wrap3");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
+
+function myFunn3() {
+  var custDiv = document.getElementById("not-wrap3");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
+
+function myFunny3() {
+  var custDiv = document.getElementById("eg-wrap3");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
+
+function myFun4() {
+  var custDiv = document.getElementById("vid-wrap4");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
+
+function myFunn4() {
+  var custDiv = document.getElementById("not-wrap4");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
+
+function myFunny4() {
+  var custDiv = document.getElementById("eg-wrap4");
+  if (custDiv.style.display === "none") {
+    custDiv.style.display = "block";
+  } else {
+    custDiv.style.display = "none";
+  }
+}
